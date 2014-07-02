4b8602df38b16b6ea9d5564a9628200b7d46678b
ssut <ssut@ssut.me>
2014-07-02 11:23:20 +0900

    Fix: use localStorage when action is relative path

--- src/location.js
+++ src/location.js
@@ -1,4 +1,10 @@
 function getLocation(href) {
+    // If href starts with relative path
+    if (href.slice(0, 1) === '/' ||
+        (href.slice(0, 5) !== 'http:' && href.slice(0, 6) !== 'https:')) {
+        current = getLocation(location.href);
+        href = current.protocol + '//' + current.host + href;
+    }
     var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/?[^?#]*)(\?[^#]*|)(#.*|)$/);
     return match && {
         protocol: match[1],
